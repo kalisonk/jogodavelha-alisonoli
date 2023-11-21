8265d04be3407f0ac783cb98f10965e7a25ee93c
kalisonk <106752910+kalisonk@users.noreply.github.com>
2023-11-21 15:53:10 -0300

    final

--- jogo/jogo.js
+++ jogo/jogo.js
@@ -6,3 +6,33 @@ let turno;
 
 const JOGADOR_X = "X";
 const JOGADOR_O = "O";
+
+let jogoAcabou = false;
+
+document.addEventListener("click", (event) => {
+    if (event.target.mathes(".celula") && !jogoAcabou) {
+
+        jogar(event.target.id);
+    }
+})
+
+function jogar(id) {
+    const celula = document.getElementById(id);
+
+    if (celula.textContent === "") {
+
+        turno = checarTurno ? JOGADOR_X : JOGADOR_O;
+
+        checarTurno = !checarTurno;
+
+        celula.textContent = turno;
+
+        celula.classList.add(turno);
+
+        celula.removeEventListener("click", jogar),
+
+        verificarVendedor(turno);
+    
+    }
+}
+
